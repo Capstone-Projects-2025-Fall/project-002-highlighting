eb6fc94c66a63896cfbb432e550876642b95fedf
Kevin <kevin.hitch@temple.edu>
2025-10-10 14:33:35 -0400

    added jest and configured

--- smartspeech/frontend/setupJest.js
+++ smartspeech/frontend/setupJest.js
@@ -1,1 +1,13 @@
 
+    // jest.config.ts
+    import type { Config } from '@jest/types';
+
+    const config: Config.InitialOptions = {
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      collectCoverage: true, // Enable code coverage collection
+      coverageDirectory: 'coverage', // Specify the output directory for coverage reports
+      coverageReporters: ['html', 'text'], // Output HTML and text reports
+    };
+
+    export default config;
