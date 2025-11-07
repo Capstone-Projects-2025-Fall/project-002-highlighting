f399a883018b6af7c07d9d298f2581595fac165c
tuh14497 <tuh14497@temple.edu>
2025-11-07 15:05:17 -0500

    remove extra chatbar

--- smartspeech/frontend/src/pages/index.tsx
+++ smartspeech/frontend/src/pages/index.tsx
@@ -73,7 +73,6 @@ export default function Home() {
             </HealthCheckProvider>
 
             <div className="fixed bottom-4 right-4 bg-white border shadow-lg p-4 rounded w-[300px] z-50">
-                <p className="font-bold mb-2">Audio Transcription</p>
                 <AudioTranscription />
             </div>
 
